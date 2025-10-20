638ab7933ca6d871ba634fe64053af99f5a5e728
luanalopez20 <luanaslopez@outlook.com>
2025-10-20 12:06:12 -0300

    Error en el get

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -3,7 +3,6 @@ import os
 import pytest
 
 # Agrega la carpeta backend al path para que Python pueda encontrar app.py
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import create_app
 
